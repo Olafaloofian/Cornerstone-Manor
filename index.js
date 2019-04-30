8fd86bb6adc38abd7a2a8d445d5e64f7d181be83
olafaloofian <bulluffalo@gmail.com>
2019-04-29 20:29:45 -0700

    Change prices for floor plans

--- index.js
+++ index.js
@@ -283,7 +283,7 @@ const planBInfo = `
     <br>
     <div class='prompt accent medium lightgreen'>
         <div>1218 Square Feet</div>
-        <div>$950 + tax</div>
+        <div>$950 - $1050 + tax</div>
     </div>
 `
 
@@ -296,7 +296,7 @@ const planCInfo = `
     <br>
     <div class='prompt accent medium lightgreen'>
         <div>1254 Square Feet</div>
-        <div>$950 + tax</div>
+        <div>$950 - $1050 + tax</div>
     </div>
 `
 let selectedImages = 'community'
